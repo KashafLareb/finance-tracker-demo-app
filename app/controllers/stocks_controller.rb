50bfe559b1251b34006e12fd1964602752dbcc5d
KashafLareb <kashaflareb@gmail.com>
2022-04-19 16:30:25 +0500

    Use ajax for form submission

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,11 +3,14 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock == nil
-        flash[:alert] = "Symbol not found! Please enter a valid sybmol."
+        # respond_to do |format|
+          flash.now[:alert] = "Symbol not found! Please enter a valid sybmol."
+          # format.js { render partial: 'users/result' }
+        # end
       end
     else
-      flash[:alert] = "You didn't enter anything! Please enter a sybmol to search."
+      flash.now[:alert] = "You didn't enter anything! Please enter a sybmol to search."
+      # format.js { render partial: 'users/result' }
     end
-    render 'users/my_portfolio'
   end
 end
