470167b6f597e96ddf951d0d30c528784d04e7f7
KashafLareb <kashaflareb@gmail.com>
2022-04-19 13:06:23 +0500

    Handle exception of invalid stock symbol entry

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,13 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_lookup(params[:stock])
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock == nil
+        flash[:alert] = "Symbol not found! Please enter a valid sybmol."
+      end
+    else
+      flash[:alert] = "You didn't enter anything! Please enter a sybmol to search."
+    end
     render 'users/my_portfolio'
   end
 end
