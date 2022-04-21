0bde5ce671b71cc8f40667444eb622d2bced4642
KashafLareb <kashaflareb@gmail.com>
2022-04-21 15:18:32 +0500

    Add my_friends page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+
+  def index
+    @all_users = User.all
+    @friends = current_user.friends
+    @not_friends = @all_users.where.not(id: @friends.ids)
+    @other_users = @not_friends.where.not(id: current_user.id)
+  end
+
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
+  def my_friends
+    @friends = current_user.friends
+  end
+
 end
