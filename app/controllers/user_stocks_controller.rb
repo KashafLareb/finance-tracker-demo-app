9dc10a928dc1e2f0d29aedfbbde208ea463b8e45
KashafLareb <kashaflareb@gmail.com>
2022-04-21 09:07:48 +0500

    Provide remove tracked stock option to user

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -4,10 +4,18 @@ class UserStocksController < ApplicationController
     if stock.blank?
       stock = Stock.new_lookup(params[:ticker])
       stock.save
-      @user_stock = UserStock.create(user: current_user, stock: stock)
-      flash[:notice] = "Stock #{stock.name} successfully added to your portfolio!"
-      
     end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Stock '#{stock.name}' successfully added to your portfolio!"
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "Stock '#{stock.name}' successfully removed!"
+    redirect_to my_portfolio_path
+  end
+
 end
